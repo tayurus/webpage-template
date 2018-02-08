cf5b77f028dd2370c9494bb8f9138fee658be00d
tayurus <tayurus@gmail.com>
2018-02-08 11:05:58 +0300

    add some features from frontend checklist

--- src/gulpFile.js
+++ src/gulpFile.js
@@ -3,20 +3,21 @@ let jade = require("gulp-jade");
 let autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
+let cleanCSS = require('gulp-clean-css');
 
 // SOURCES
 let src = {
     css: ["**/*.css", "!node_modules/**/*.css"],
     js: ["**/*.js", "!node_modules/**/*.js", "!gulpFile.js"],
     pages: ["pages/**/*.jade", "blocks/**/*.jade"],
-    img: ["**/*.png", "**/*.jpg", "**/*.svg"],
+    img: ["**/*.png", "**/*.jpg", "**/*.svg", "**/*.ico"],
 }
 
 let dist = {
     main: "../dist/",
     img: "../dist/img"
 }
- 
+
 gulp.task("build", function() {
     //PAGES
     gulp.src(src.pages).pipe(jade({
@@ -32,6 +33,7 @@ gulp.task("build", function() {
     let autoprefixBrowsers = ['> 1%', 'last 2 versions', 'firefox >= 4', 'safari 7', 'safari 8', 'IE 8', 'IE 9', 'IE 10', 'IE 11'];
     gulp.src(src.css)
         .pipe(concat("styles.css"))
+        .pipe(cleanCSS())
         .pipe(autoprefixer({
             browsers: autoprefixBrowsers
         }))
---
+++ src/readme.txt
@@ -0,0 +1,13 @@
+File structure descripition:
+	blocks - BEM blocks
+	css - common css
+	fonts - common fonts
+	img - common img
+	js - common js
+	pages - website pages
+	gulpFile - intructions to build project using node.js
+
+To run project do:
+1. npm install
+2. gulp build
+3. go to folder "../dist" and open "index.html"
